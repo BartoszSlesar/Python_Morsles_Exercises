b6a85ad2257d1d6d75816bedbcddc093187affaf
BartoszSlesar <BartoszSlesar@gmail.com>
2020-07-25 11:22:14 +0200

    git_delete_unused

--- Zadanie2/earliest.py
+++
@@ -1,8 +0,0 @@
-def get_earliest(date1, date2):
-    tdate1 = [int(el) for el in date1.split("/")]
-    tdate2 = [int(el) for el in date2.split("/")]
-    for element in range(-1,2,1):
-        if(tdate1[element] > tdate2[element]):
-            return date2
-        elif tdate1[element] < tdate2[element]:
-            return date1
